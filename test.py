3e0d2fc7d9e12c0b990d19c34cf4ddcd61f4f66a
samueltv250 <samueltv250@vt.edu>
2023-11-12 04:57:34 -0500

    ensured this isulated record fucntion works independantly

--- test.py
+++ test.py
@@ -1,68 +1,84 @@
 import os
-import pickle
 import numpy as np
 import SoapySDR as sdr
-from datetime import datetime, timedelta
-DATA_BASE_DIR = "/home/dietpi/Desktop/MDE/data_base"
+from datetime import datetime
 
-def record(sdr_device, frequency, gain, sample_rate, duration_seconds):
+DATA_BASE_DIR = "/mnt/usbdrive"
+
+class SDRRecorder:
+    def __init__(self, device_args, mode='single'):
+        self.device = sdr.Device(device_args)
+        self.mode = mode
+        self.sample_rate = 2e6  # Same sample rate for both modes for simplicity
+        self.streams = [None, None] if mode == 'dual' else [None]  # Adjusted for mode
+        self.is_initialized = False
+
+    def setup_device(self, channel, frequency, gain):
+        self.device.setSampleRate(sdr.SOAPY_SDR_RX, channel, self.sample_rate)
+        self.device.setFrequency(sdr.SOAPY_SDR_RX, channel, frequency)
+        self.device.setGain(sdr.SOAPY_SDR_RX, channel, gain)
+
+    def activate_stream(self, channel):
+        if self.streams[channel] is None:
+            self.streams[channel] = self.device.setupStream(sdr.SOAPY_SDR_RX, sdr.SOAPY_SDR_CF32, [channel])
+            self.device.activateStream(self.streams[channel])
+
+    def record(self, channel, duration_seconds):
+        rx_stream = self.streams[channel]
+        num_samples = int(self.sample_rate * duration_seconds)
+        buffer_size = 1024  # Smaller buffer size to avoid overflows
+        buff = np.empty(buffer_size, dtype=np.complex64)
+
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        filename = f"SDR_Channel{channel}_{timestamp}.pkl"
+        file_path = os.path.join(DATA_BASE_DIR, filename)
+
+        with open(file_path, 'wb') as file:
+            samples_collected = 0
+            while samples_collected < num_samples:
+                sr = self.device.readStream(rx_stream, [buff], buffer_size)
+                if sr.ret > 0:
+                    np.save(file, buff[:sr.ret])
+                    samples_collected += sr.ret
+                elif sr.ret == -1:
+                    print("Overflow occurred")
+                    break
+
+    def start_recording(self, frequency, gain, duration_seconds):
+        for channel in range(len(self.streams)):
+            self.setup_device(channel, frequency, gain)
+            self.activate_stream(channel)
+
+        # Recording in a sequential manner to avoid thread issues
+        for channel in range(len(self.streams)):
+            self.record(channel, duration_seconds)
+
+        # Deactivate and close streams after recording
+        for channel, stream in enumerate(self.streams):
+            if stream is not None:
+                self.device.deactivateStream(stream)
+                self.device.closeStream(stream)
 
 
-
-
-    # Set up the SDR device parameters
-    sdr_device.setSampleRate(sdr.SOAPY_SDR_RX, 0, sample_rate)
-    sdr_device.setFrequency(sdr.SOAPY_SDR_RX, 0, frequency)
-    sdr_device.setGain(sdr.SOAPY_SDR_RX, 0, gain)
-
-    # Create a stream
-    rx_stream = sdr_device.setupStream(sdr.SOAPY_SDR_RX, sdr.SOAPY_SDR_CF32)
-    sdr_device.activateStream(rx_stream)
-
-    # Calculate the total number of samples to capture
-    num_samples = int(sample_rate * duration_seconds)
-
-    # Create a buffer to hold the samples
-    buffer_size = 65536
-    buff = np.empty(buffer_size, dtype=np.complex64)
-
-    # Determine the filename based on the current time
-    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    filename = f"SDR_{frequency}Hz_{timestamp}.pkl"
-    file_path = os.path.join(DATA_BASE_DIR, filename)
-
-    # Open the file for writing
-    with open(file_path, 'ab') as file:
-        samples_collected = 0
-        while samples_collected < num_samples:
-            # Read samples from the device
-            sr = sdr_device.readStream(rx_stream, [buff], buffer_size)
-            if sr.ret > 0:
-                # Store samples in the file
-                # pickle.dump(buff[:sr.ret], file)
-                samples_collected += sr.ret
-            elif sr.ret == -1:
-                # Overflow has occurred
-                print("Overflow occurred")
-                break
-
-    # Deactivate and close the stream
-    sdr_device.deactivateStream(rx_stream)
-    sdr_device.closeStream(rx_stream)
-
-
+# Example usage
 devices = sdr.Device.enumerate()
+single_device_args = None
+dual_device_args = None
+
 for dev in devices:
     print(dev)
-    if dev["label"].strip() == "SDRplay Dev0 RSPduo 230102CE34 - Single Tuner":
+    if "Single Tuner" in dev["label"]:
         single_device_args = dev
-    elif dev["label"].strip() == "SDRplay Dev1 RSPduo 230102CE34 - Dual Tuner":
+    elif "Dual Tuner" in dev["label"]:
         dual_device_args = dev
 
-# Example usage:
-mainSdr = sdr.Device(single_device_args) # Initialize your SoapySDR Device here
-frequency = 1.626e9  # Frequency in Hz
-gain = 30  # Gain in dB
-sample_rate = 2e6  # Sample rate in samples per second
-duration_seconds = 120  # Duration of recording in seconds
-record(mainSdr, frequency, gain, sample_rate, duration_seconds)
+
+
+
+if dual_device_args:
+    dual_tuner_recorder = SDRRecorder(dual_device_args, mode='dual')
+    dual_tuner_recorder.start_recording(1.626e9, 30, 10)
+
+if single_device_args:
+    single_tuner_recorder = SDRRecorder(single_device_args, mode='single')
+    single_tuner_recorder.start_recording(1.626e9, 30, 10)
